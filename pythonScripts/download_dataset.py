9423767b36d05d6379a6d7aba61206e57a524ceb
katebrich <katebrich@gmail.com>
2020-07-30 14:42:39 +0200

    edit download_dataset. Merged with filter_dataset.

--- pythonScripts/download_dataset.py
+++ pythonScripts/download_dataset.py
@@ -7,46 +7,107 @@ import threading
 import traceback
 import shutil
 import getopt
-from functions import eprint, restAPI_get
+from functions import eprint, restAPI_get, get_entity_id
+import uuid
+from Bio import SeqIO
+from Bio.PDB import PDBParser, PDBIO
+from Bio.PDB.PDBIO import Select
 
+class ChainSelect(Select):
+    def __init__(self, chain):
+        super(Select, self).__init__()
+        self.chain = chain
+    def accept_residue(self, residue):
+        if residue.parent.id ==self.chain and residue.id[0] != 'W': #todo filtrovani ligandu
+            return 1
+        else:
+            return 0
+    def accept_model(self, model):
+        if model.id == 0:
+            return 1
+        else:
+            return 0
 
-def download_file(url, output_path):
+def get_PDB(temp_file, out_dir, pdb_id, chain_ids, ligands_filter=None):
+    #in_pdb_file_path = os.path.join(in_dir, pdb_id + ".pdb") #todo windows?
+    url = f'https://www.ebi.ac.uk/pdbe/entry-files/pdb{pdb_id}.ent'
     response = restAPI_get(url)
-    with open(output_path, 'wb') as file:
+    with open(temp_file, 'wb') as file:
         file.write(response)
+    parser = PDBParser(PERMISSIVE=0, QUIET=1)  # todo
+    structure = parser.get_structure(pdb_id, temp_file)
+    chains_list = []
+    if (chain_ids == '*'):
+        for chain in structure.get_chains():
+            chains_list.append(chain.id)
+    else:
+        chains_list = chain_ids.split(',')
+    for chain_id in chains_list: #todo chain_id = *
+        out_pdb_file_path = os.path.join(out_dir, pdb_id + chain_id + ".pdb")
+        io = PDBIO()
+        io.set_structure(structure)
+        io.save(out_pdb_file_path, ChainSelect(chain_id), preserve_atom_numbering=True)
+
+
+def get_FASTA(temp_file, out_dir, pdb_id, chain_ids): #todo zapsat tam i ten header?
+    if (chain_ids == '*'): # get all chains
+        url = f"http://www.ebi.ac.uk/pdbe/entry/pdb/{pdb_id}/fasta"
+        response = restAPI_get(url)
+        with open(temp_file, 'wb') as file:
+            file.write(response)
+        records = list(SeqIO.parse(temp_file, "fasta"))
+        for record in records:
+            ids = record.description.split('|')
+            chains = ids[2].split(' ')
+            for chain_id in chains:
+                sequence = record.seq
+                out_fasta_file_path = os.path.join(out_dir, pdb_id + chain_id + ".fasta")
+                with open(out_fasta_file_path, 'w') as file:
+                    # todo vypsat i header?
+                    file.write(str(sequence.upper()))
+    else:
+        chains = chain_ids.split(',')
+        for chain_id in chains:
+            entity_id = get_entity_id(pdb_id, chain_id) #todo mit udelany cache
+            url = f"https://www.ebi.ac.uk/pdbe/entry/pdb/{pdb_id}/fasta?entity={entity_id}"
+            response = restAPI_get(url)
+            out_fasta_file_path = os.path.join(out_dir, pdb_id + chain_id + ".fasta")
+            with open(out_fasta_file_path, 'wb') as file:
+                file.write(response)
 
 def download_structure(str):
     try:
         pdb_id = str[0]
         chain_ids = str[1]
-
-        # get .pdb file
-        url=f'https://www.ebi.ac.uk/pdbe/entry-files/pdb{pdb_id}.ent'
-        output_path = os.path.join(output_dir, "PDB", pdb_id + ".pdb") #, chain_id)
+        #PDB
+        temp_file = f"temp_{uuid.uuid1()}"
         try:
-            download_file(url, output_path)
+            get_PDB(temp_file, output_PDB, pdb_id, chain_ids, ligands_filter)
         except:
             eprint(f"ERROR: downloading PDB file - {pdb_id}") #todo test jestli se dostane do chyboveho vystupu shell skriptu
             raise
-
-        # get .fasta file
-        url=f"http://www.ebi.ac.uk/pdbe/entry/pdb/{pdb_id}/fasta"
-        output_path = os.path.join(output_dir, "FASTA", pdb_id + ".fasta") #chain_id)
+        finally:
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
+        #FASTA
         try:
-            download_file(url, output_path)
-        except: #t
+            #download_file(url, output_path)
+            get_FASTA(temp_file, output_FASTA, pdb_id, chain_ids)
+        except:
             eprint(f"ERROR: downloading FASTA file - {pdb_id}")
-
+            raise
+        finally:
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
         #todo vypisovani, ale aby to bylo nejak rychlejsi
         with threadLock:
             global global_counter
             idx = global_counter
             global_counter += 1
-
-        print(f"{idx}/{total}: {pdb_id} downloaded")
-    except:
-        print() #todo test
-        # todo log error structures
+        print(f"{idx}/{total}: {pdb_id} {chain_ids} downloaded")
+    except Exception as e:
+        eprint(f"ERROR: downloading {pdb_id} {chain_ids}: {traceback.print_exc(e)}") #todo test
+        # todo log error structures, count
 
 
 dataset_file = ""
@@ -71,15 +132,18 @@ for opt, arg in opts:
 if (dataset_file == ""):
     eprint("ERROR: Dataset must be specified.") #todo psat z jakeho skriptu je chyba
     sys.exit(1)
+#output_dir = os.path.join(os.path.dirname(dataset_file), f"test_{uuid.uuid1()}")  #todo jen pro debugovani
 if (output_dir == ""):
     eprint("ERROR: Output directory must be specified.")
     sys.exit(1)
 
-#remove output directory and its contents, if it exists
+output_PDB = os.path.join(output_dir, "PDB")
+output_FASTA = os.path.join(output_dir, "FASTA")
+
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-os.makedirs(f"{output_dir}/FASTA")
-os.makedirs(f"{output_dir}/PDB")
+os.makedirs(output_PDB)
+os.makedirs(output_FASTA) #todo co kdyz existuje?
 
 structures = parse_dataset(dataset_file)
 
--- pythonScripts/filter_dataset.py
+++ pythonScripts/filter_dataset.py
@@ -1,90 +1,23 @@
 from parse_dataset import parse_dataset
-from Bio import SeqIO
-from Bio.PDB import PDBParser, PDBIO
-from Bio.PDB.PDBIO import Select
+
 import os as os
 import shutil
-from functions import get_fasta_path
+from functions import restAPI_get, get_entity_id
+import uuid
 
-#todo parametry
+
+
+
+
+
+
+
+
 dataset_name = "joined(mlig)"
 dataset_file = f'/home/katebrich/Documents/diplomka/datasets/{dataset_name}.txt'
-#dataset_dir = f"/home/katebrich/Documents/diplomka/datasets/{dataset_name}"
+dataset_dir = f"/home/katebrich/Documents/diplomka/datasets/{dataset_name}"
 
 out_files_list = []
-
-class ChainSelect(Select):
-    def __init__(self, chain):
-        super(Select, self).__init__()
-        self.chain = chain
-    def accept_residue(self, residue):
-        if residue.parent.id ==self.chain and residue.id[0] != 'W':
-            return 1
-        else:
-            return 0
-    def accept_model(self, model):
-        if model.id == 0:
-            return 1
-        else:
-            return 0
-
-def filter_PDB(in_dir, out_dir, pdb_id, chain_ids, filter_ligands=True):
-    in_pdb_file_path = os.path.join(in_dir, pdb_id + ".pdb") #todo windows?
-    parser = PDBParser(PERMISSIVE=0, QUIET=1)  # todo
-    structure = parser.get_structure(pdb_id, in_pdb_file_path)
-    chains_list = []
-    if (chain_ids == '*'):
-        for chain in structure.get_chains():
-            chains_list.append(chain.id)
-    else:
-        chains_list = chain_ids.split(',')
-    #todo model
-    for chain_id in chains_list: #todo chain_id = *
-        out_pdb_file_path = os.path.join(out_dir, pdb_id + chain_id + ".pdb")
-        io = PDBIO()
-        io.set_structure(structure)
-        io.save(out_pdb_file_path, ChainSelect(chain_id), preserve_atom_numbering=True)
-        out_files_list.append(out_pdb_file_path)
-
-def filter_FASTA(in_dir, out_dir, pdb_id, chain_ids):
-    in_fasta_file_path = os.path.join(in_dir, pdb_id + ".fasta") #todo windows?
-    try:
-        records = list(SeqIO.parse(in_fasta_file_path, "fasta"))
-    except Exception as e:
-        print(f"Error: {pdb_id} {chain_ids}")
-        print(str(e))
-        return
-
-    #todo
-    #if (chain_ids == '*'):
-    #   for chain in structure.get_chains():
-    #        chains_list.append(chain.id)
-    #else:
-    chains_list = chain_ids.split(',')
-
-    for record in records:
-        ids = record.description.split('|')
-        chains = ids[2].split(' ')
-        for chain_id in chains:
-            if chain_id in chains_list:
-                sequence = record.seq
-                chains_list.remove(chain_id)
-                out_fasta_file_path = os.path.join(out_dir, pdb_id + chain_id + ".fasta")
-                with open(out_fasta_file_path, 'w') as file:
-                    #todo vypsat i header?
-                    file.write(str(sequence.upper()))
-    if len(chains_list) != 0:
-        print(f"Error: sequence not found for {pdb_id} {chains_list}")  # todo
-
-    ## create temp file
-    #temp_file_path = file_path + ".temp"
-    #with open(temp_file_path, 'w') as file:
-    #    file.write(str(sequence.upper()))
-    ## remove original file
-    #os.remove(file_path)
-    ## rename temp file
-    #os.rename(temp_file_path, file_path)
-
 
 dataset = parse_dataset(dataset_file)
 
@@ -109,11 +42,11 @@ for structure in dataset:
     chain_ids = structure[1]
 
     # filter FASTA file - leave only sequence for this chain
-    filter_FASTA(in_dir_fasta, out_dir_fasta, pdb_id, chain_ids)
+    get_FASTA(in_dir_fasta, out_dir_fasta, pdb_id, chain_ids)
 
 
     # filter PDB - leave only these chains + filter relevant ligands
-    filter_PDB(in_dir_pdb, out_dir_pdb, pdb_id, chain_ids, True)
+    get_PDB(in_dir_pdb, out_dir_pdb, pdb_id, chain_ids, True)
 
     print(f"{i}/{total}: {pdb_id} {chain_ids} processed")
 
