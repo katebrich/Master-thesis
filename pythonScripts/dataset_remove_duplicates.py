461d8de67e2e290516a8eaeb0b4f68c0ca19d7ab
katebrich <katebrich@gmail.com>
2020-11-01 13:07:05 +0100

    chen11 duplicates removed

---
+++ pythonScripts/dataset_remove_duplicates.py
@@ -0,0 +1,16 @@
+import os
+from collections import Counter
+
+from helper import parse_dataset, parse_dataset_split_chains
+import re
+from Bio.PDB import *
+
+dataset_name = "holo4k"
+datasets_dir = f"/home/katebrich/Documents/diplomka/GitHub/datasets/"
+dataset_path = f"{datasets_dir}/{dataset_name}.txt"
+
+
+dataset = parse_dataset_split_chains(dataset_path)
+
+print(Counter(dataset))
+
